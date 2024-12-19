71264f33e5a1cae0dede1310af04f14aee85688c
TakamasaSugiura <takamasas2008@gmail.com>
2024-12-19 11:04:44 +0900

    add Dockerfile and scripts

---
+++ V1/ubuntu24.04/setup_remoteengine.sh
@@ -0,0 +1,224 @@
+#!/bin/sh
+set -e
+
+CONFIG_DIR=/config
+CONFIG_FILE=$CONFIG_DIR/remoteengine.yml
+
+escape_str () {
+    sed 's/"/\\"/g' | sed 's/\$/\\\$/g'
+}
+
+if [ ! -f $CONFIG_FILE ]
+then
+    exit 0
+fi
+
+ROOT=$(yq ".remote-engine" $CONFIG_FILE)
+
+if [ -z "$ROOT" ]
+then
+    echo "no remote-engine element"
+    exit 1
+fi
+
+# password
+PASSWORD=$(yq ".remote-engine.password // \"\"" $CONFIG_FILE)
+if [ -z "$PASSWORD" ]
+then
+    PASSWORD=$REMOTEENGINE_PASSWORD
+fi
+if [ ! -z "$PASSWORD" ]
+then
+    orin3.remoteengine changepassword -n "$PASSWORD"
+fi
+
+# endpoints
+ENDPOINT_COUNT=$(yq ".remote-engine.endpoints | length" $CONFIG_FILE)
+i=0
+while [ $i -lt $ENDPOINT_COUNT ]
+do
+    ep_ip=$(yq ".remote-engine.endpoints[$i].ip" $CONFIG_FILE)
+    ep_port=$(yq ".remote-engine.endpoints[$i].port" $CONFIG_FILE)
+    ep_pfx=$(yq ".remote-engine.endpoints[$i].pfx // \"\"" $CONFIG_FILE)
+    ep_password=$(yq ".remote-engine.endpoints[$i].password // \"\"" $CONFIG_FILE)
+    ep_client_auth=$(yq ".remote-engine.endpoints[$i].cleint-auth // \"\"" $CONFIG_FILE)
+
+    command="orin3.remoteengine ep add $ep_ip --port $ep_port"
+    if [ ! -z "$ep_pfx" ]
+    then
+        escaped_pfx=$(echo $ep_pfx | escape_str)
+        escaped_password=$(echo $ep_password | escape_str)
+        command="$command --pfx \"$CONFIG_DIR/$escaped_pfx\" --password \"$escaped_password\""
+        if [ "$ep_client_auth" == "yes" ]
+        then
+            command="$command --client-auth"
+        fi
+    fi
+    eval $command
+    i=$(($i+1))
+done
+
+# opentelemetry
+OPENTELEMETRY_COUNT=$(yq ".remote-engine.opentelemerties | length" $CONFIG_FILE)
+i=0
+while [ $i -lt $OPENTELEMETRY_COUNT ]
+do
+    ot_url=$(yq ".remote-engine.opentelemerties[$i].url" $CONFIG_FILE)
+    ot_metric=$(yq ".remote-engine.opentelemerties[$i].metric" $CONFIG_FILE)
+    ot_log=$(yq ".remote-engine.opentelemerties[$i].log" $CONFIG_FILE)
+    ot_trace=$(yq ".remote-engine.opentelemerties[$i].trace" $CONFIG_FILE)
+    ot_proxy=$(yq ".remote-engine.opentelemerties[$i].proxy // \"\"" $CONFIG_FILE)
+    ot_protocol=$(yq ".remote-engine.opentelemerties[$i].protocol // \"\"" $CONFIG_FILE)
+
+    command="orin3.remoteengine ot add $ot_url"
+    if [ "$ot_metric" == "yes" ]
+    then
+        command="$command -m"
+    fi
+    if [ "$ot_log" == "yes" ]
+    then
+        command="$command -l"
+    fi
+    if [ "$ot_trace" == "yes" ]
+    then
+        command="$command -t"
+    fi
+    if [ ! -z "$ot_proxy" ]
+    then
+        if [ "$ot_proxy" == "system" ]
+        then
+            command="$command --use-system-proxy"
+        elif [ "$ot_proxy" == "none" ]
+        then
+            command="$command --no-proxy"
+        else
+            command="$command --proxy \"$ot_proxy\""
+        fi
+    fi
+    if [ ! -z "$ot_protocol" ]
+    then
+        command="$command --protocol $ot_protocol"
+    fi
+    eval $command
+    i=$(($i+1))
+done
+
+# loglevel
+LOGLEVEL=$(yq ".remote-engine.loglevel // \"\"" $CONFIG_FILE)
+if [ ! -z "$LOGLEVEL" ]
+then
+    orin3.remoteengine ot setloglevel $LOGLEVEL
+fi
+
+# provider(install)
+PROV_INSTALL_COUNT=$(yq ".remote-engine.provider.install_paths | length" $CONFIG_FILE)
+i=0
+while [ $i -lt $PROV_INSTALL_COUNT ]
+do
+    prov_install_file=$(yq ".remote-engine.provider.install_paths[$i].file" $CONFIG_FILE)
+    escaped_file=$(echo $prov_install_file | escape_str)
+    orin3.remoteengine prov install "$CONFIG_DIR/$escaped_file"
+
+    i=$(($i+1))
+done
+
+# provider(attach)
+PROV_ATTACH_COUNT=$(yq ".remote-engine.provider.attach_paths | length" $CONFIG_FILE)
+i=0
+while [ $i -lt $PROV_ATTACH_COUNT ]
+do
+    prov_attach_path=$(yq ".remote-engine.provider.attach_paths[$i].path" $CONFIG_FILE)
+    escaped_path=$(echo $prov_attach_path | escape_str)
+    orin3.remoteengine prov attach "$escaped_path"
+
+    i=$(($i+1))
+done
+
+# provider(pfx)
+PROV_PFX_COUNT=$(yq ".remote-engine.provider.pfxes | length" $CONFIG_FILE)
+i=0
+while [ $i -lt $PROV_PFX_COUNT ]
+do
+    prov_pfx_file=$(yq ".remote-engine.provider.pfxes[$i].file" $CONFIG_FILE)
+    prov_pfx_ip=$(yq ".remote-engine.provider.pfxes[$i].ip" $CONFIG_FILE)
+    prov_pfx_password=$(yq ".remote-engine.provider.pfxes[$i].password // \"\"" $CONFIG_FILE)
+    escaped_pfx=$(echo $prov_pfx_file | escape_str)
+    command="orin3.remoteengine prov addpfx $prov_pfx_ip --pfx \"$CONFIG_DIR/$escaped_pfx\""
+    if [ ! -z "$prov_pfx_password" ]
+    then
+        escaped_password=$(echo $prov_pfx_password | escape_str)
+        command="$command --password \"$escaped_password\""
+    fi
+    eval $command
+    i=$(($i+1))
+done
+
+# manual endpoints
+ENDPOINT_COUNT=$(yq ".remote-engine.manual-endpoints | length" $CONFIG_FILE)
+i=0
+while [ $i -lt $ENDPOINT_COUNT ]
+do
+    manep_ip=$(yq ".remote-engine.manual-endpoints[$i].ip" $CONFIG_FILE)
+    manep_port=$(yq ".remote-engine.manual-endpoints[$i].port" $CONFIG_FILE)
+    manep_pfx=$(yq ".remote-engine.manual-endpoints[$i].pfx // \"\"" $CONFIG_FILE)
+    manep_password=$(yq ".remote-engine.manual-endpoints[$i].password // \"\"" $CONFIG_FILE)
+
+    command="orin3.remoteengine manep add $manep_ip --port $manep_port"
+    if [ ! -z "$manep_pfx" ]
+    then
+        escaped_pfx=$(echo $manep_pfx | escape_str)
+        escaped_password=$(echo $manep_password | escape_str)
+        command="$command --pfx \"$CONFIG_DIR/$escaped_pfx\" --password \"$escaped_password\""
+        if [ "$manep_client_auth" == "yes" ]
+        then
+            command="$command --client-auth"
+        fi
+    fi
+    eval $command
+    i=$(($i+1))
+done
+
+# authorities
+AUTHORITY_COUNT=$(yq ".remote-engine.authorities | length" $CONFIG_FILE)
+if [ $AUTHORITY_COUNT -gt 0 ]
+then
+    lines=$(expr $(orin3.remoteengine auth ls --password $PASSWORD | sed '/^\s*$/d' | wc -l) - 1)
+    i=0
+    while [ $i -lt $lines ]
+    do
+        orin3.remoteengine auth delete --password $PASSWORD --index 0 -y
+        i=$(($i+1))
+    done
+    i=0
+    while [ $i -lt $AUTHORITY_COUNT ]
+    do
+        auth_name=$(yq ".remote-engine.authorities[$i].name" $CONFIG_FILE)
+        auth_effect=$(yq ".remote-engine.authorities[$i].effect" $CONFIG_FILE)
+        auth_condition=$(yq ".remote-engine.authorities[$i].condition // \"\"" $CONFIG_FILE)
+        auth_target=$(yq ".remote-engine.authorities[$i].target // \"\"" $CONFIG_FILE)
+
+        escaped_password=$(echo $PASSWORD | escape_str)
+        command="orin3.remoteengine auth add --password \"$escaped_password\" --name $auth_name --effect $auth_effect"
+        if [ ! -z "$auth_condition" ]
+        then
+            command="$command --condition \"$auth_condition\""
+        fi
+        if [ ! -z "$auth_target" ]
+        then
+            command="$command --target \"$auth_target\""
+        fi
+        eval $command
+        i=$(($i+1))
+    done
+fi
+
+# licenses
+LICENSE_COUNT=$(yq ".remote-engine.licenses | length" $CONFIG_FILE)
+i=0
+while [ $i -lt $LICENSE_COUNT ]
+do
+    lic_key=$(yq ".remote-engine.authorities[$i].key" $CONFIG_FILE)
+
+    orin3.remoteengine lic activate $lic_key
+    i=$(($i+1))
+done
